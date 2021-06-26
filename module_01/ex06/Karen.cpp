1f92b59bdb1cd0393fcd1302165a1bdcf546cf6e
gauthier poulain <gapoulai@student.42lyon.fr>
2021-06-26 17:45:45 +0200

    done ex06

---
+++ module_01/ex06/Karen.cpp
@@ -0,0 +1,61 @@
+#include "Karen.hpp"
+#include <iostream>
+
+typedef void (Karen::*pfunc)(void);
+
+void Karen::debug(void)
+{
+	std::cout << "[ DEBUG ]" << std::endl
+			  << "I love to get extra baconfor my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl
+			  << std::endl;
+}
+
+void Karen::info(void)
+{
+	std::cout << "[ INFO ]" << std::endl
+			  << "I cannot believe adding extrabacon cost more money. You don’t put enough! If you did I would not have to askfor it!" << std::endl
+			  << std::endl;
+}
+
+void Karen::warning(void)
+{
+	std::cout << "[ WARNING ]" << std::endl
+			  << "I think I deserve to have some extra bacon for free. I’ve beencoming here for years and you just started working here last month." << std::endl
+			  << std::endl;
+}
+
+void Karen::error(void)
+{
+	std::cout << "[ ERROR ]" << std::endl
+			  << "This is unacceptable, I want to speak to the manager now." << std::endl
+			  << std::endl;
+}
+
+void Karen::complain(std::string filter, std::string level)
+{
+	typedef void (Karen::*p_pfunc)(void);
+	bool print;
+
+	std::string type[]{
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"};
+	p_pfunc func[] = {
+		&Karen::debug,
+		&Karen::info,
+		&Karen::warning,
+		&Karen::error};
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (type[i] == filter)
+			print = true;
+		if (type[i] == level)
+		{
+			if (print)
+				(this->*func[i])();
+			break;
+		}
+	}
+}
---
+++ module_01/ex06/main.cpp
@@ -0,0 +1,38 @@
+#include "Karen.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	Karen karen;
+	std::string filter;
+	bool in_types;
+
+	if (argc < 2)
+		filter = "DEBUG";
+	else
+		filter = argv[1];
+
+	in_types = false;
+	std::string type[]{
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"};
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (filter == type[i])
+			in_types = true;
+	}
+	if (!in_types)
+	{
+		std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl
+				  << std::endl;
+		return 0;
+	}
+
+	karen.complain(filter, "DEBUG");
+	karen.complain(filter, "INFO");
+	karen.complain(filter, "WARNING");
+	karen.complain(filter, "ERROR");
+	return 0;
+}
