058dc29a357ddc558a32b2394349189e0ee6119b
Gauthier Poulain <gapoulai@student.42lyon.fr>
2021-07-15 13:21:26 +0200

    fix day 01

--- module_01/ex00/Zombie.cpp
+++ module_01/ex00/Zombie.cpp
@@ -3,16 +3,19 @@
 
 Zombie::Zombie()
 {
+	std::cout << "a new zombie is \"alive\"" << std::endl;
 }
 
 Zombie::Zombie(std::string name)
 {
 	p_name = name;
+	std::cout << p_name << " is \"alive\"" << std::endl;
 }
 
 Zombie::~Zombie()
 {
 	this->announce();
+	std::cout << p_name << " is dead" << std::endl;
 }
 
 void Zombie::setName(std::string name)
@@ -22,5 +25,5 @@ void Zombie::setName(std::string name)
 
 void Zombie::announce(void)
 {
-	std::cout << p_name << " est mort comme un turbo looser" << std::endl;
+	std::cout << p_name << " BraiiiiiiinnnzzzZ..." << std::endl;
 }
