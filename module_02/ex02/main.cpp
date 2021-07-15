1df213af3fc6e2929350eaa4c5faba6eeb645749
Gauthier Poulain <gapoulai@student.42lyon.fr>
2021-07-15 13:24:44 +0200

    add main test

--- module_02/ex02/main.cpp
+++ module_02/ex02/main.cpp
@@ -4,6 +4,19 @@
 int main(void)
 {
 	{
+		std::cout << "base subject test" << std::endl;
+		Fixed a;
+		Fixed const b(Fixed(5.05f) * Fixed(2));
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << Fixed::max(a, b) << std::endl;
+		std::cout << std::endl;
+	}
+	{
 		std::cout << "indrement/decrement test" << std::endl;
 
 		Fixed a;
