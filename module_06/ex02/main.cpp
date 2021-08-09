f2cffeed5d0def7facc812379409caf1ba64ec0c
gauthier poulain <poulain.gauthier16@gmail.com>
2021-08-09 17:02:19 +0200

    ça marche avec les pointeurs

--- module_06/ex02/main.cpp
+++ module_06/ex02/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *generate(void)
+{
+	srand(time(NULL));
+	switch (rand() % 3)
+	{
+	case 0:
+		std::cout << "generate new A" << std::endl;
+		return (new A);
+	case 1:
+		std::cout << "generate new B" << std::endl;
+		return (new B);
+	case 2:
+		std::cout << "generate new C" << std::endl;
+		return (new C);
+	default:
+		return (0);
+	}
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C" << std::endl;
+}
+
+// void identify(Base &p)
+// {
+// }
 
 int main(void)
 {
-
+	Base *ptr = generate();
+	identify(ptr);
+	delete ptr;
 	return (0);
 }
