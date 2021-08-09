0053e4b8896a57424c539a54109261c577b4e4d8
gauthier poulain <poulain.gauthier16@gmail.com>
2021-08-09 17:11:38 +0200

    maintenant aussi sans

--- module_06/ex02/main.cpp
+++ module_06/ex02/main.cpp
@@ -36,14 +36,42 @@ void identify(Base *p)
 		std::cout << "C" << std::endl;
 }
 
-// void identify(Base &p)
-// {
-// }
+void identify(Base &p)
+{
+	try
+	{
+		Base &tmp = dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+		return (void)tmp;
+	}
+	catch (const std::exception &e)
+	{
+	}
+	try
+	{
+		Base &tmp = dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+		return (void)tmp;
+	}
+	catch (const std::exception &e)
+	{
+	}
+	try
+	{
+		Base &tmp = dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+		return (void)tmp;
+	}
+	catch (const std::exception &e)
+	{
+	}
+}
 
 int main(void)
 {
 	Base *ptr = generate();
 	identify(ptr);
+	identify(*ptr);
 	delete ptr;
 	return (0);
 }
