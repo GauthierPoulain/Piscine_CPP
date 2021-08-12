d1fe6181237a82c02d20d756d0875d1ae220695f
gauthier poulain <poulain.gauthier16@gmail.com>
2021-08-12 14:00:04 +0200

    c'est bon la normalement

--- module_08/ex02/mutantstack.hpp
+++ module_08/ex02/mutantstack.hpp
@@ -3,7 +3,7 @@
 
 #include <stack>
 #include <iterator>
-#include <list>
+#include <vector>
 
 template <typename T>
 class MutantStack : public std::stack<T>
@@ -17,5 +17,39 @@ public:
 	iterator begin();
 	iterator end();
 };
+
+template <typename T>
+MutantStack<T>::MutantStack()
+{
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack &src) : std::stack<T>(src)
+{
+}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &src)
+{
+	std::stack<T>::operator=(src);
+	return *this;
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return this->c.end();
+}
 
 #endif
