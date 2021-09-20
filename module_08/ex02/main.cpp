a828be6e1f0f509c93095a8c3938e10a08d3df7a
Gauthier Poulain <gapoulai@student.42lyon.fr>
2021-09-20 14:40:18 +0200

    cassé mais ok

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -4,35 +4,56 @@
 #include <stack>
 #include "mutantstack.hpp"
 
+// int main()
+// {
+// 	MutantStack<int> mstack;
+
+// 	mstack.push(5);
+// 	mstack.push(17);
+
+// 	std::cout << "top " << mstack.top() << std::endl;
+
+// 	mstack.pop();
+
+// 	std::cout << "size " << mstack.size() << std::endl;
+
+// 	mstack.push(3);
+// 	mstack.push(5);
+// 	mstack.push(737);
+// 	//[...]
+// 	mstack.push(0);
+
+// 	MutantStack<int>::iterator it = mstack.begin();
+// 	MutantStack<int>::iterator ite = mstack.end();
+
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+// 	std::stack<int> s(mstack);
+// 	return 0;
+// }
+
 int main()
 {
-	MutantStack<int> mstack;
-
-	mstack.push(5);
-	mstack.push(17);
-
-	std::cout << "top " << mstack.top() << std::endl;
-
-	mstack.pop();
-
-	std::cout << "size " << mstack.size() << std::endl;
-
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-
-	++it;
-	--it;
-	while (it != ite)
+	try
 	{
-		std::cout << *it << std::endl;
-		++it;
+		MutantStack<int> mstack;
+
+		mstack.push(10);
+
+		MutantStack<int>::iterator it = mstack.begin();
+
+		std::cout << &(it) << std::endl;
+
+		MutantStack<int>::iterator ite = mstack.end();
+		std::cout << &(ite) << std::endl;
 	}
-	std::stack<int> s(mstack);
-	return 0;
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
--- module_08/ex02/mutantstack.cpp
+++ module_08/ex02/mutantstack.cpp
@@ -1,1 +1,44 @@
-// :)
+#include "mutantstack.hpp"
+
+// template <typename T>
+template <class T>
+MutantStack<T>::MutantStack()
+{
+}
+
+// template <typename T>
+template <class T>
+MutantStack<T>::MutantStack(const MutantStack &src) : std::stack<T>(src)
+{
+}
+
+// template <typename T>
+template <class T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &src)
+{
+	std::stack<T>::operator=(src);
+	return *this;
+}
+
+// template <typename T>
+template <class T>
+MutantStack<T>::~MutantStack()
+{
+}
+
+// template <typename T>
+template <class T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return this->c.begin();
+}
+
+// template <typename T>
+template <class T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return this->c.end();
+}
+
+
+https://stackoverflow.com/questions/115703/storing-c-template-function-definitions-in-a-cpp-file
