807d9a5669104361a0ec084d78abbed1caaf7903
gauthier poulain <poulain.gauthier16@gmail.com>
2021-09-21 03:53:52 +0200

    finish 08

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -6,33 +6,63 @@
 
 int main()
 {
-	MutantStack<int> mstack;
-
-	mstack.push(5);
-	mstack.push(17);
-
-	std::cout << "top " << mstack.top() << std::endl;
-
-	mstack.pop();
-
-	std::cout << "size " << mstack.size() << std::endl;
-
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+
+		std::cout << "top " << mstack.top() << std::endl;
+
+		mstack.pop();
+
+		std::cout << "size " << mstack.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
+	{
+		MutantStack<std::string> mstack;
+
+		mstack.push("salut");
+		mstack.push("bonjour");
+
+		std::cout << "top " << mstack.top() << std::endl;
+
+		mstack.pop();
+
+		std::cout << "size " << mstack.size() << std::endl;
+
+		mstack.push("presentement");
+		mstack.push("yo");
+		mstack.push("coucou");
+		mstack.push("wesh");
+
+		MutantStack<std::string>::iterator it = mstack.begin();
+		MutantStack<std::string>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<std::string> s(mstack);
+	}
 	return 0;
 }
--- module_08/ex02/mutantstack.hpp
+++ module_08/ex02/mutantstack.hpp
@@ -18,4 +18,38 @@ public:
 	iterator end();
 };
 
+template <typename T>
+MutantStack<T>::MutantStack()
+{
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack &src) : std::stack<T>(src)
+{
+}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &src)
+{
+	std::stack<T>::operator=(src);
+	return *this;
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return this->c.end();
+}
+
 #endif
